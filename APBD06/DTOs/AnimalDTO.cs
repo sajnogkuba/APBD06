5aae92610e19251d92d4349dc5328733e3bf693e
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-15 23:08:07 +0200

    add endpoint to add animals

--- APBD06/DTOs/AnimalDTO.cs
+++ APBD06/DTOs/AnimalDTO.cs
@@ -1,3 +1,4 @@
 namespace APBD06.DTOs;
 
 public record GetAllAnimalsResponse(int Id, string Name, string? Description, string Category, string Area);
+public record CreateAnimalRequest(string Name, string? Description, string Category, string Area);
--- APBD06/Program.cs
+++ APBD06/Program.cs
@@ -22,36 +22,41 @@ app.UseHttpsRedirection();
 app.MapGet("animals/", (IConfiguration configuration, string? orderBy = "") =>
 {
     var animals = new List<GetAllAnimalsResponse>();
-    using (var sqlConnection = new SqlConnection(configuration.GetConnectionString("Default")))
-    {
-        string sqlText;
-        var allowedOrderBy = new List<string?> { "IdAnimal", "Name", "Description", "Category", "Area" };
-        if (allowedOrderBy.Contains(orderBy))
-        {
-            sqlText = $"SELECT * FROM Animal ORDER BY {orderBy}";
-        }
-        else
-        {
-            sqlText = "SELECT * FROM Animal ORDER BY Name";
-        }
+    using var sqlConnection = new SqlConnection(configuration.GetConnectionString("Default"));
+    var allowedOrderBy = new List<string?> { "IdAnimal", "Name", "Description", "Category", "Area" };
+    var sqlText = allowedOrderBy.Contains(orderBy) ? $"SELECT * FROM Animal ORDER BY {orderBy}" :
+        "SELECT * FROM Animal ORDER BY Name";
         
 
-        var sqlCommand = new SqlCommand(sqlText, sqlConnection);
-        sqlCommand.Parameters.AddWithValue("@orderBY", orderBy);
-        sqlCommand.Connection.Open();
-        var sqlDataReader = sqlCommand.ExecuteReader();
-        while (sqlDataReader.Read())
-        {
-            animals.Add(new GetAllAnimalsResponse(
-                sqlDataReader.GetInt32(0),
-                sqlDataReader.GetString(1),
-                sqlDataReader.IsDBNull(2) ? null : sqlDataReader.GetString(2),
-                sqlDataReader.GetString(3),
-                sqlDataReader.GetString(4)));
+    var sqlCommand = new SqlCommand(sqlText, sqlConnection);
+    sqlCommand.Connection.Open();
+    var sqlDataReader = sqlCommand.ExecuteReader();
+    while (sqlDataReader.Read())
+    {
+        animals.Add(new GetAllAnimalsResponse(
+            sqlDataReader.GetInt32(0),
+            sqlDataReader.GetString(1),
+            sqlDataReader.IsDBNull(2) ? null : sqlDataReader.GetString(2),
+            sqlDataReader.GetString(3),
+            sqlDataReader.GetString(4)));
 
-        }
-
-        return Results.Ok(animals);
     }
+    return Results.Ok(animals);
+});
+
+app.MapPost("animals/", (IConfiguration configuration, CreateAnimalRequest request) =>
+{
+    using var sqlConnection = new SqlConnection(configuration.GetConnectionString("Default"));
+    var sqlCommand = new SqlCommand(
+        "INSERT INTO Animal (Name, Description, Category, Area) VALUES (@name, @desc, @category, @area)",
+        sqlConnection);
+    sqlCommand.Parameters.AddWithValue("@name", request.Name);
+    sqlCommand.Parameters.AddWithValue("@desc", request.Description);
+    sqlCommand.Parameters.AddWithValue("@category", request.Category);
+    sqlCommand.Parameters.AddWithValue("@area", request.Area);
+    sqlCommand.Connection.Open();
+    sqlCommand.ExecuteNonQuery();
+
+    return Results.Created("", null);
 });
 app.Run();
